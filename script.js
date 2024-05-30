57a8ecb04180fa892c12676a8600ddf2aeeb5e5e
Vinayka awasthi <147977944+vinayka2601@users.noreply.github.com>
2024-05-30 11:57:19 +0530

    first commit
    firstlive

---
+++ script.js
@@ -0,0 +1,43 @@
+gsap.from("#img1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+   y:60,
+})
+gsap.from("#img2",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+   x:60,
+})
+gsap.from("#img3",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+   y:-60,
+})
+gsap.from("#main>h1 ",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+ 
+})
+gsap.from("#main>h2",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+ 
+})
+const img =document.querySelectorAll("img");
+img.forEach(function(img){
+    img.addEventListener("mouseenter",function(){
+        gsap.to(img,{
+            y:-20,
+        });
+    });
+    img.addEventListener("mouseleave",function(){
+        gsap.to(img,{
+            y:0,
+        });
+    });
+});
